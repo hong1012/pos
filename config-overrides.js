42a8951adab366d8cf83a7c3be0ad6637ca45a8e
yaohong_zhou <yaohong_zhou@kingdee.com>
2018-09-05 09:55:45 +0800

    优化

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+/**
+ * Created by hong on 2018/9/4.
+ */
+
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = function override(config, env) {
+  config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],   config);
+  config = rewireLess.withLoaderOptions({ modifyVars: { "@primary-color": "#1DA57A" } })(config, env);
+  return config;
+};
